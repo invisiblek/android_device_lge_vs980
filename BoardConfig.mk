57ff0485525676976aac1148784ce0ce631b17b4
Jackpot Clavin <jonclavin@gmail.com>
2014-09-16 19:04:15 -0500

    cmdline: add panel info to please the bootloader
    Change-Id: I813b013eddd07b36cfa685cd64bb6c1fd3d7b7cd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,9 @@ include device/lge/g2-common/BoardConfigCommon.mk
 
 TARGET_KERNEL_CONFIG := cyanogenmod_vs980_defconfig
 
+#   ** this cmdline is overriding the one in g2-common **
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g2 user_debug=31 msm_rtb.filter=0x0 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd
+
 TARGET_OTA_ASSERT_DEVICE := vs980,g2,galbi
 
 G2_DTS_TARGET := msm8974-g2-vzw
